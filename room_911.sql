b05607927e3027fe7ab62355229d2085406ec3b4
ledezmamauricio17 <73148960+ledezmamauricio17@users.noreply.github.com>
2022-11-14 16:13:08 -0500

    BD

---
+++ room_911.sql
@@ -0,0 +1,312 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-11-2022 a las 22:06:15
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `room_911`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departments`
+--
+
+CREATE TABLE `departments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `departments`
+--
+
+INSERT INTO `departments` (`id`, `name`) VALUES
+(1, 'DEPARTMENT 2'),
+(2, 'DEPARTMENT 1'),
+(3, 'DEPARTMENT 3');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_09_17_204951_create_departments_table', 1),
+(2, '2014_10_12_000000_create_users_table', 1),
+(3, '2014_10_12_100000_create_password_resets_table', 1),
+(4, '2019_08_19_000000_create_failed_jobs_table', 1),
+(5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(6, '2022_09_18_204951_create_records_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `expires_at` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `records`
+--
+
+CREATE TABLE `records` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `action` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `records`
+--
+
+INSERT INTO `records` (`id`, `action`, `date`, `user_id`) VALUES
+(1, 'login', '2022-11-11 17:09:16', 2),
+(2, 'login', '2022-11-11 12:11:49', 2),
+(3, 'login', '2022-11-11 12:12:25', 1),
+(4, 'entry', '2022-11-11 12:21:57', 2),
+(5, 'entry', '2022-11-11 12:22:08', 1),
+(6, 'entry', '2022-11-12 03:42:17', 1),
+(7, 'entry', '2022-11-12 14:57:02', 1),
+(8, 'entry', '2022-11-12 18:31:56', 2),
+(9, 'entry', '2022-11-12 18:33:07', 2),
+(10, 'entry', '2022-11-12 18:33:31', 2),
+(11, 'entry', '2022-11-12 18:34:28', 2),
+(12, 'try', '2022-11-12 18:47:59', 2),
+(13, 'try', '2022-11-12 21:55:39', 1),
+(14, 'try', '2022-11-12 21:57:26', 1),
+(15, 'try', '2022-11-12 22:30:30', 2),
+(16, 'try', '2022-11-12 22:40:43', 2),
+(17, 'try', '2022-11-12 22:41:26', 5),
+(18, 'try', '2022-11-12 22:41:30', 5),
+(19, 'try', '2022-11-12 22:42:46', 5),
+(20, 'entry', '2022-11-12 22:53:23', 1),
+(21, 'entry', '2022-11-12 22:54:57', 7),
+(22, 'entry', '2022-11-12 22:59:48', 7),
+(23, 'try', '2022-11-12 23:25:18', 2),
+(24, 'entry', '2022-11-12 23:26:27', 1),
+(25, 'entry', '2022-11-13 13:37:57', 1),
+(26, 'entry', '2022-11-13 21:03:58', 1),
+(27, 'try', '2022-11-14 13:24:48', 1),
+(28, 'try', '2022-11-14 13:25:03', 1),
+(29, 'try', '2022-11-14 13:28:09', 1),
+(30, 'try', '2022-11-14 13:29:15', 5),
+(31, 'try', '2022-11-14 13:29:23', 5),
+(32, 'try', '2022-11-14 13:37:39', 1),
+(33, 'try', '2022-11-14 13:37:39', 1),
+(34, 'try', '2022-11-14 13:38:49', 1),
+(35, 'try', '2022-11-14 13:50:51', 2),
+(36, 'entry', '2022-11-14 13:51:09', 1),
+(37, 'entry', '2022-11-14 16:03:55', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `document` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `department_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `document`, `type`, `password`, `status`, `department_id`) VALUES
+(1, 'Mauricio', 'Ledezma', '1002', '2', '$2y$10$Pau0.SIQNokN86uPJ97EMuyfXAztenZvAq4QnaJm/XMe7na4l14NW', '1', 1),
+(2, 'stiv', 'julian', '1004', '1', '$2y$10$.ss650lfrHa7YgyJEkPFluVKttaeDlPjhSj1VZRDP5iejQNbI8uxK', '0', 3),
+(7, 'asdasa', 'm m', '5555', '2', '$2y$10$.ss650lfrHa7YgyJEkPFluVKttaeDlPjhSj1VZRDP5iejQNbI8uxK', '1', 3),
+(19, 'samuel', 'sanchez', '10452', '1', NULL, '1', 2),
+(20, 'michell', 'gonzalez', '30989', '2', '$2y$10$Nsz4GFtleGovedWmSCsAHORA2rbBvZlSewihk/xk/ptpuEJgDj7nq', '1', 2),
+(21, 'sara', 'palacios', '1002943', '1', NULL, '1', 1),
+(28, 'brayan', 'lozano', '455676', '2', '$2y$10$gpW8teh4G9.ur.lYZfUziupTDrla7xbICLL45EIqzACMEWHjU4azy', '0', 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indices de la tabla `records`
+--
+ALTER TABLE `records`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `records_user_id_foreign` (`user_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_document_unique` (`document`),
+  ADD KEY `users_department_id_foreign` (`department_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `records`
+--
+ALTER TABLE `records`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `records`
+--
+ALTER TABLE `records`
+  ADD CONSTRAINT `records_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_department_id_foreign` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
